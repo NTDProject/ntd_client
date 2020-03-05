05d621683b033a8c4fdc9e9036bf37759011ad38
quoc95 <quoc95pro@gmail.com>
2020-03-05 21:52:12 +0700

    init

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+import users from '../pages/admin/Login/reducers';
+import partnerInfo from '../pages/partner/Account/reducers';
+import product from '../pages/partner/Product/reducers';
+
+const appReducers = combineReducers({
+    users,
+    partnerInfo,
+    product
+});
+
+export default appReducers;
